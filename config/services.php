ed51676b156451b3961dd3b035888be8b96f9885
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2023-07-26 23:07:57 +0100

    Add scopes to config

--- config/services.php
+++ config/services.php
@@ -4,6 +4,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT')
+        'redirect' => env('GOOGLE_REDIRECT'),
+        'scopes' => [
+            \Google\Service\Sheets::DRIVE,
+            \Google\Service\Sheets::SPREADSHEETS
+        ],
+        'access_type'      => 'online',
+        'approval_prompt'  => 'auto',
+        'prompt'           => 'consent', //"none", "consent", "select_account" default:none
     ],
 ];
