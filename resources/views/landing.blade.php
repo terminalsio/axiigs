1587687157ed448b2e72ecbb1bd019a559dc42fc
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2023-07-26 13:47:17 +0100

    Fix landing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="flex justify-center">
+                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">✨ Axii</h2>
+            </div>
+        </div>
+    </div>
+@endsection
